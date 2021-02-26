085947734888b111282111035446f2f8815c4991
chenhaoxuan <38680050@qq.com>
2021-02-26 22:03:09 +0800

    补充启动类注释

--- src/main/java/com/wsbxd/excel/formula/calculation/ExcelFormulaCalculationApplication.java
+++ src/main/java/com/wsbxd/excel/formula/calculation/ExcelFormulaCalculationApplication.java
@@ -3,6 +3,13 @@ package com.wsbxd.excel.formula.calculation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * description: Excel 公式计算 Application
+ *
+ * @author chenhaoxuan
+ * @version 1.0
+ * @date 2021/2/26 22:01
+ */
 @SpringBootApplication
 public class ExcelFormulaCalculationApplication {
 
